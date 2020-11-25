f09e5cec5dc6160b0286fff091fa92799ede9cf6
RgMellon <renan_gian@hotmail.com>
2020-11-25 20:55:45 -0300

    terminando css do menu

--- src/pages/PerfilUser/index.js
+++ src/pages/PerfilUser/index.js
@@ -1,24 +1,44 @@
 import React from "react";
 
+import { MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
+
 import * as S from "./styles";
 
 const PerfilUser = () => {
   return (
     <S.Container>
       <S.ContentImage>
-          <S.CircleImage></S.CircleImage> 
-          <S.TextNome>Nome Cuidador</S.TextNome>         
-      </S.ContentImage> 
+        <S.CircleImage>
+          <MaterialIcons name="photo-camera" size={30} color="#FFC249" />
+        </S.CircleImage>
+        <S.TextNome>Nome Cuidador</S.TextNome>
+      </S.ContentImage>
 
       <S.Detail />
 
-      <S.TextInput>
-          <S.Text>Me torna anfitrião</S.Text>
-      </S.TextInput>
-      <S.TextInput >
-          <S.Text>Edita perfil</S.Text>
-      </S.TextInput>
+      <S.Content>
+        <S.MenuItem>
+          <FontAwesome5 solid name="paw" size={24} color="#999" />
 
+          <S.Info>
+            <S.Text>Me torna anfitrião</S.Text>
+            <S.Description> Faça parte da comunidade </S.Description>
+          </S.Info>
+
+          <FontAwesome5 solid name="angle-right" size={18} color="#FFC249" />
+        </S.MenuItem>
+
+        <S.MenuItem>
+          <FontAwesome5 solid name="address-card" size={24} color="#999" />
+
+          <S.Info>
+            <S.Text>Edita perfil</S.Text>
+            <S.Description> Edite seu perfil </S.Description>
+          </S.Info>
+
+          <FontAwesome5 solid name="angle-right" size={18} color="#FFC249" />
+        </S.MenuItem>
+      </S.Content>
     </S.Container>
   );
 };
--- src/pages/PerfilUser/styles.js
+++ src/pages/PerfilUser/styles.js
@@ -1,56 +1,80 @@
 import styled from "styled-components/native";
 
 export const Container = styled.ScrollView`
-    flex: 1;
-    background: #fff;
-    
+  flex: 1;
+  background: #fff;
 `;
 
 export const ContentImage = styled.View`
-    width: 100%;
-    height: 200px;
-    background: #c6d3d5;
+  width: 100%;
+  height: 200px;
+  background: #c6d3d5;
 
-    justify-content: center;
-    align-items: center;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const CircleImage = styled.View`
-    width: 120px;
-    height: 120px;
-    background: #fff;
-    border-radius: 60px;
-    margin-top: 15px;
+export const CircleImage = styled.TouchableOpacity`
+  width: 100px;
+  height: 100px;
+  background: #fff;
+  border-radius: 60px;
+  margin-top: 15px;
+
+  justify-content: center;
+  align-items: center;
 `;
 
 export const TextNome = styled.Text`
-    text-align: center;
-    color: #fff;
-    font-weight: bold;
-    font-size: 18px;
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
+  font-size: 17px;
+  margin-top: 8px;
 `;
 
 export const Detail = styled.View`
-    width: 100%;
-    height: 20px;
-    background: #fff;
-    margin-top: -15px;
-    border-top-left-radius: 40px;
-    border-top-right-radius: 40px;
+  width: 100%;
+  height: 20px;
+  background: #fff;
+  margin-top: -15px;
+  border-top-left-radius: 40px;
+  border-top-right-radius: 40px;
 `;
 
-export const TextInput = styled.TouchableOpacity`
-    width: 320px;
-    height: 50px;
+export const MenuItem = styled.TouchableOpacity`
+  width: 100%;
+  border-bottom-width: 0.5px;
+  border-color: #999999;
+  margin-top: 40px;
+  padding-left: 10px;
 
-    border-radius: 8px;
-    border-bottom-width: 1px;
-    margin-top: 40px;
-    padding-left: 10px;
+  flex-direction: row;
+  align-items: center;
 
+  justify-content: space-between;
 `;
 
 export const Text = styled.Text`
-    margin-top: 10px;
+  margin-top: 10px;
+  font-size: 17px;
+`;
 
+export const Content = styled.View`
+  align-items: center;
+  margin-top: 10px;
+  padding: 20px;
+`;
+
+export const Description = styled.Text`
+  color: #999999;
+  font-size: 12px;
+  margin-top: 3px;
+  margin-bottom: 10px;
+  margin-left: -2px;
+`;
+
+export const Info = styled.View`
+  margin-left: -50px;
+  width: 200px;
 `;
